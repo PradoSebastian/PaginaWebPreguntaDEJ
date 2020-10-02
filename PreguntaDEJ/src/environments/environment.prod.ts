f27902492145944a935835e78d33d2273c854116
Juan Sebastián Prado Valero <pradosebas@yahoo.com.co>
2020-10-02 13:32:09 -0500

    Inicio

---
+++ PreguntaDEJ/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig = {
+    apiKey: "AIzaSyBo38v9XNnMWqqlZ7OzldtS8Elu-cm4s4w",
+    authDomain: "paginawebpreguntadej-fe94b.firebaseapp.com",
+    databaseURL: "https://paginawebpreguntadej-fe94b.firebaseio.com",
+    projectId: "paginawebpreguntadej-fe94b",
+    storageBucket: "paginawebpreguntadej-fe94b.appspot.com",
+    messagingSenderId: "633760194403",
+    appId: "1:633760194403:web:2b6225c50b1985f6133d56"
+  };
+};
---
+++ PreguntaDEJ/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig = {
+    apiKey: "AIzaSyBo38v9XNnMWqqlZ7OzldtS8Elu-cm4s4w",
+    authDomain: "paginawebpreguntadej-fe94b.firebaseapp.com",
+    databaseURL: "https://paginawebpreguntadej-fe94b.firebaseio.com",
+    projectId: "paginawebpreguntadej-fe94b",
+    storageBucket: "paginawebpreguntadej-fe94b.appspot.com",
+    messagingSenderId: "633760194403",
+    appId: "1:633760194403:web:2b6225c50b1985f6133d56"
+  };
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
