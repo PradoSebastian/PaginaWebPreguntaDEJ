6aabc9cb8ba9be33e223ca3b00ffc90df197810d
Juan Sebastián Prado Valero <pradosebas@yahoo.com.co>
2020-10-09 11:00:52 -0500

    Dependencias @angular/fire

--- PreguntaDEJ/src/app/app.module.ts
+++ PreguntaDEJ/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {AngularFireModule} from '@angular/fire';
+import { environment } from '../environments/environment.prod';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
